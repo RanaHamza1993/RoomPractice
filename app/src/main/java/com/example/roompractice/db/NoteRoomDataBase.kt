11f9ca573daa77ec7bfb5239bca678b66284c94e
ranahamza2014 <rana.hamza2014@gmail.com>
2020-01-25 14:49:46 +0500

    push

--- app/src/main/java/com/example/roompractice/db/NoteRoomDataBase.kt
+++ app/src/main/java/com/example/roompractice/db/NoteRoomDataBase.kt
@@ -1,9 +1,31 @@
 package com.example.roompractice.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.roompractice.db.entities.Note
+import com.example.roompractice.db.daos.NoteDao
+import com.example.roompractice.db.entities.NoteEntity
 
-@Database(entities =Note::class.java ,version = 1)
+@Database(entities = [NoteEntity::class],version = 1)
 abstract class NoteRoomDataBase :RoomDatabase() {
+
+    abstract fun noteDao():NoteDao
+    @Volatile
+    private var noteRoomInstance: NoteRoomDataBase? = null
+    internal fun getDatabase(context: Context): NoteRoomDataBase {
+        if (noteRoomInstance == null) {
+            synchronized(NoteRoomDataBase::class.java) {
+                if (noteRoomInstance == null) {
+                    noteRoomInstance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NoteRoomDataBase::class.java, "note_database"
+                    )
+                        .build()
+                }
+            }
+        }
+        return noteRoomInstance!!
+    }
+
 }
--- app/src/main/java/com/example/roompractice/db/entities/NoteEntity.kt
+++ app/src/main/java/com/example/roompractice/db/entities/NoteEntity.kt
@@ -3,6 +3,6 @@ package com.example.roompractice.db.entities
 import androidx.room.Entity
 
 @Entity(tableName = "notes")
-class Note{
+class NoteEntity{
 
 }
